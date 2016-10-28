6fb509dfa2863a18613b6de15746e20bcb9e1eba
lodysg <lodysg@gmail.com>
2016-10-28 18:59:52 +0200

    clean up

--- govaserver.js
+++ govaserver.js
@@ -6,9 +6,13 @@ var io = require('socket.io')(server);
 var request = require('request');
 var last_action_date = Date.now();
 var isControlled = false;
-var robot = require("./robot.js");
 
-console.log(robot);
+var five = require("johnny-five");
+var board = new five.Board({ 
+    port: "/dev/ttyACM0",
+    repl: false,
+    debug: false
+});
 
 var port = 5360;
 
@@ -19,21 +23,31 @@ app.get('/', function(req, res) {
     res.render(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
-    socket.on('speed', function(speed) {
-        robot.setSpeed(speed);
-    });
+board.on("ready", function() {
+        var motorLeft = new five.Motor({ pins: { pwm: 6, dir: 7 }, invertPWM: true }); // à revoir
+        var motorRight = new five.Motor({ pins: { pwm: 11, dir: 12 }, invertPWM: true }); // à revoir
+        
+        var move = (speedObj) => {
+            if (speedObj.left > 0)
+                motorLeft.forward(speedObj.left);
+            else if (speedObj.left < 0)
+                motorLeft.reverse(Math.abs(speedObj.left));
+            else
+                motorLeft.stop();
+
+            if (speedObj.right > 0)
+                motorRight.forward(speedObj.right);
+            else if (speedObj.right < 0)
+                motorRight.reverse(Math.abs(speedObj.right));
+            else
+                motorRight.stop();
+        };
+
+        io.sockets.on('connection', (socket) => {
+            socket.on('speed', (speed) => {
+                move(speed);
+            });
+        });
 });
 
-/*
-setInterval(function(){
-    var n = Date.now();
-    //console.log(last_action_date);
-    if(((n - last_action_date) > 300) && true){
-        
-    }
-},1000);
-*/
-
-robot.start();
 server.listen(port, () => console.log("app launched -> localhost:" + port));
